e367258c9b37f0494bdc9d36a6a48adb62f9c94f
Sjors Provoost <sjors@sprovoost.nl>
2024-07-09 09:53:13 +0200

    Remove unused CRPCSignals
    They are no longer used for anything since RPCNotifyBlockChange was replaced with waitTipChanged() from the mining interface.

--- src/init.cpp
+++ src/init.cpp
@@ -427,15 +427,6 @@ static void registerSignalHandler(int signal, void(*handler)(int))
 }
 #endif
 
-static void OnRPCStarted()
-{
-}
-
-static void OnRPCStopped()
-{
-    LogPrint(BCLog::RPC, "RPC stopped.\n");
-}
-
 void SetupServerArgs(ArgsManager& argsman)
 {
     SetupHelpOptions(argsman);
@@ -704,8 +695,6 @@ static void StartupNotify(const ArgsManager& args)
 static bool AppInitServers(NodeContext& node)
 {
     const ArgsManager& args = *Assert(node.args);
-    RPCServer::OnStarted(&OnRPCStarted);
-    RPCServer::OnStopped(&OnRPCStopped);
     if (!InitHTTPServer(*Assert(node.shutdown))) {
         return false;
     }
